78b913c4694919edcea167be3086dea7da48e31b
BarbaraVillena <barbimorevi@gmail.com>
2026-06-17 23:54:42 -0300

    Local Storage - Villena Luisana Bárbara

---
+++ pv_tp_integrador_gp16/src/context/AdminContext.jsx
@@ -0,0 +1,42 @@
+import { createContext, useState, useEffect } from "react";
+export const AdminContext = createContext(null);
+
+export const AdminProvider = ({ children }) => {
+
+  const [AdminActivo, setAdminActivo] = useState(() => {
+    const adminGuardado = localStorage.getItem("adminActivo");
+
+  if (adminGuardado) {
+    return JSON.parse(adminGuardado);
+  }
+
+  return null;
+});
+
+useEffect(() => {
+  localStorage.setItem(
+    "adminActivo",
+    JSON.stringify(adminActivo)
+  );
+}, [adminActivo]);
+
+  const guardarSesion = (admin) => {
+    setAdminActivo(admin);
+  };
+
+  const cerrarSesion = () => {
+    setAdminActivo(null);
+  };
+
+  return (
+    <AdminContext.Provider
+      value={{
+        adminActivo,
+        guardarSesion,
+        cerrarSesion
+      }}
+    >
+      {children}
+    </AdminContext.Provider>
+  );
+};
