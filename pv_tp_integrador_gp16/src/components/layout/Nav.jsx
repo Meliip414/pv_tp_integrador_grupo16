b0987e658cf46d2fe50c9e0ca40b80d0f0abaa47
apzzoe <apazazoe8@gmail.com>
2026-06-18 01:53:47 -0300

    Agregado contenido a nav.jsx principal -Zelaya Apaza Zoe

--- pv_tp_integrador_gp16/src/components/layout/Nav.jsx
+++ pv_tp_integrador_gp16/src/components/layout/Nav.jsx
@@ -3,9 +3,6 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
 
-
-
-
 function Navegacion() {
   return (
     <Navbar expand="lg" bg="light">
